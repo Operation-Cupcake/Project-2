efc17142471dedc8611f3cdf466a0bd66504fc53
iDenatra03 <dantar@hawk.iit.edu>
2025-04-07 14:42:17 -0500

    fixed js to make hamburger work

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.querySelector('.menu-icon');
     const menu = document.querySelector('.hamburger-menu');
-  
+
     menuIcon.addEventListener('click', () => {
-      menu.classList.toggle('show');
+        console.log("Clicked")
+        menu.classList.toggle("show-menu")
     });
-  });
-  
+});
