9b4ea5d3d4f7619d11960dcf44e082d9a8b98f8c
iDenatra03 <dantar@hawk.iit.edu>
2025-04-07 14:33:48 -0500

    fixing js code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.hamburger-menu');
-
-menuIcon.addEventListener('click', () => {
-  menu.classList.toggle('show-menu');
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menu = document.querySelector('.hamburger-menu');
+  
+    menuIcon.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
+  });
+  
